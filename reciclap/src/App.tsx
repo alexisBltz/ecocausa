ae4df29f1bf41f95f820c277a353999013c848b5
alexisBltz <almamanima@unsa.edu.pe>
2024-11-30 18:46:29 -0500

    modificacion del priyecto, agregando nuevas dependencias

---
+++ reciclap/src/App.tsx
@@ -0,0 +1,31 @@
+
+
+import './App.css'
+import {Header} from "./components/header.tsx";
+import {Hero} from "@/components/hero.tsx";
+
+import {BrowserRouter} from "react-router-dom";
+import {Footer} from "@/components/footer.tsx";
+import {ContactForm} from "@/components/contact-form.tsx";
+import {Marketplace} from "@/components/marketplace.tsx";
+import {Mission} from "@/components/mission.tsx";
+function App() {
+  return (
+    <>
+        <BrowserRouter>
+            <div className="min-h-screen bg-white">
+                <Header/>
+                <main>
+                    <Hero/>
+                    <Mission/>
+                    <Marketplace/>
+                    <ContactForm/>
+                </main>
+                <Footer/>
+            </div>
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
